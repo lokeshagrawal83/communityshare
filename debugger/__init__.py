980642e25af0504052f6b6e5ba5689e60cafc357
Dennis Snell <dennis.snell@automattic.com>
2016-07-22 02:47:23 -0700

    Refactoring and restyling

--- debugger/__init__.py
+++ debugger/__init__.py
@@ -1,14 +1,20 @@
 import debugger.pycharm
 
+last_connectible_debugger = None
+
+
+def init():
+    global last_connectible_debugger
+
+    last_connectible_debugger = first_available_debugger()
+
 
 def first_available_debugger():
-    for debugger in [pycharm]:
-        if debugger.can_connect():
-            return debugger
+    for nextDebugger in [pycharm]:
+        if nextDebugger.can_connect():
+            return nextDebugger
 
     return None
-
-last_connectible_debugger = first_available_debugger()
 
 
 def can_connect_to_debugger():
@@ -22,3 +28,5 @@ def inject_debugger():
         last_connectible_debugger.inject_debugger()
     else:
         raise RuntimeError('Tried connecting to debugger but none is available')
+
+init()
